2db2ea3bdc1e9289d75f8604b7bda4e2e76385bc
Drew Watson <drew@moltin.com>
2016-11-06 20:24:44 +0000

    initial commit

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+// Use strict typing - Can't pass ints to a string function etc.
+declare(strict_types = 1);
+
+// _DIR_ - always use the same relative path to the file it is used in
+require __DIR__ . '/../src/Bootstrap.php';
